d0943aad9148ce60604cc96fd71b55e8fb7bbb84
Cuonglearning <125129844+Cuonglearning@users.noreply.github.com>
2023-04-04 17:07:10 +0700

    Update CTDL&GT_Quick_Sort_1.cpp

--- CTDL&GT_Quick_Sort_1.cpp
+++ CTDL&GT_Quick_Sort_1.cpp
@@ -52,7 +52,7 @@ int main()
     int arr[] = {10, 80, 30, 90, 40, 50, 70};
     int n = sizeof(arr)/sizeof(arr[0]);
     quickSort(arr, 0, n-1);
-    printf("Sorted array: \n");
+    printf("Mang da sap xep: \n");
     printArray(arr, n);
     return 0;
 }
