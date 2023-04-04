71edf19f4bab42fe6bf11fcf85fc37f8c1559ae8
Cuonglearning <125129844+Cuonglearning@users.noreply.github.com>
2023-04-04 16:36:39 +0700

    Add files via upload

---
+++ CTDL&GT_Selection_Sort_2.cpp
@@ -0,0 +1,44 @@
+//sap xep mot mang duoc nhap tu bang phim
+#include<stdio.h>
+
+void Nhap(int a[],int n){//ham nhap mang
+	for(int i=0;i<n;i++){
+		 printf("\nNhap a[%d]=",i);
+         scanf("%d",&a[i]);
+	}
+}
+
+void Xuat(int a[],int n){//ham xuat mang
+    for(int i=0; i<n; i++){
+        printf("%d \t",a[i]);
+    }
+}
+
+void Selection_sort(int a[], int n){
+	int min_index;
+	for (int i = 0; i < n - 1; i++){
+		min_index = i;
+		for (int j = i + 1; j < n; j++){
+			if (a[min_index] > a[j]){
+				min_index = j;
+			}
+		}
+		if (i != min_index){
+			int temp = a[i];
+			a[i] = a[min_index];
+			a[min_index] = temp;
+		}
+	}
+	printf("Mang sau khi sap xep:");
+	Xuat(a,n);
+} 
+
+int main()
+{
+	int a[100], n;
+	printf("Nhap so phan tu cua mang:");
+	scanf("%d",&n);
+	Nhap(a,n);
+	Selection_sort(a,n);
+}
+
