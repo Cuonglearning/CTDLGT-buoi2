51c0aad8b8f7166e37fa1abd5625af0125bf02fa
Cuonglearning <125129844+Cuonglearning@users.noreply.github.com>
2023-04-04 15:51:52 +0700

    Add files via upload

---
+++ CTDL&GT_Interchange_Sort_2.cpp
@@ -0,0 +1,43 @@
+//sap xep mot mang duoc nhap tu bang phim
+#include <stdio.h>
+
+void nhap(int a[],int n){
+	for(int i=0;i<n;i++){
+		 printf("\nNhap a[%d]=",i);
+         scanf("%d",&a[i]);
+	}
+}
+
+void Xuat(int a[],int n){
+    for(int i=0; i<n; i++){
+        printf("%d \t",a[i]);
+    }
+}
+
+int Interchange_Sort(int a[], int n){
+	int i,j;
+	for(i=0; i<=n-1; i++){
+		for(j=i+1; j<n; j++){
+			if(a[i]>a[j]){
+				int tg = a[i];
+                a[i] = a[j];
+                a[j] = tg;	
+			}
+		}
+	}
+	printf("\nMang sau khi sap xep:\t");
+	Xuat(a,n);
+}
+
+int main()
+{
+	int a[100],n;
+	printf("Nhap so phan tu cua mang:");
+	scanf("%d",&n);
+	nhap(a,n);
+	printf("Mang ban dau:\t\t");
+	Xuat(a,n);
+	Interchange_Sort(a, n);
+}
+
+
