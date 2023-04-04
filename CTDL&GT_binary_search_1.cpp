747c7274cb2fd5fd7359f0f86af53626d1392eed
Cuonglearning <125129844+Cuonglearning@users.noreply.github.com>
2023-04-04 14:19:12 +0700

    Update CTDL&GT_binary_search_1.cpp

--- CTDL&GT_binary_search_1.cpp
+++ CTDL&GT_binary_search_1.cpp
@@ -1,32 +1,27 @@
-// Ham tim kiem nhi phan, nguon:https://blog.luyencode.net/thuat-toan-tim-kiem-nhi-phan/
-#include<stdio.h>
+// Cai dat tim kiem nhi phan / nguon:https://blog.luyencode.net/thuat-toan-tim-kiem-nhi-phan/
+#include <stdio.h>
  
-int binarySearch(int arr[], int l, int r, int x) {
-  if (r >= l) {
-    int mid = l + (r - l) / 2; 
- 
-    if (arr[mid] == x)
-      return mid;
- 
-    if (arr[mid] > x)
-      return binarySearch(arr, l, mid - 1, x);
- 	
- 	if (arr[mid] < x)
-    return binarySearch(arr, mid + 1, r, x);
-  }
- 
+int search(int arr[], int n, int x)
+{
+  int i;
+  for (i = 0; i < n; i++)
+    if (arr[i] == x)
+      
+      return i;
+
   return -1;
 }
  
-int main(void) {
-  int arr[] = {2, 3, 7, 10, 10, 40};
+int main() {
+  int arr[] = {2, 3, 4, 10, 40};
   int n = sizeof(arr) / sizeof(arr[0]);
   int x = 10;
-  int result = binarySearch(arr, 0, n - 1, x);
-  if (result == -1)
-    printf("10 khong co trong mang");
-  else
+  int result = search(arr, n, x);
+  if (result != -1) {
     printf("%d xuat hien tai chi so %d", x, result);
+  } else {
+    printf("%d khong co trong mang", x);
+  }
   return 0;
 }
 
